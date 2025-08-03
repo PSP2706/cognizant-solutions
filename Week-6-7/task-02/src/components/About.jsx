a7d969454f34567d1a0279a5bb7cdecd547a8ff4
Partha Sarathi Panda <parthasarathipanda2706@gmail.com>
2025-08-03 12:50:15 +0530

    Added task-02

---
+++ Week-6-7/task-02/src/components/About.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class About extends Component{
+    render(){
+        <div>
+        <h3>Welcome to about page of student management portal</h3>
+        </div>
+    }
+}
+
+export default About;
---
+++ Week-6-7/task-02/src/components/Home.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class Home extends Component{
+    render(){
+        <div>
+        <h3>Welcome to home page of student management portal</h3>
+        </div>
+    }
+}
+
+export default Home;
