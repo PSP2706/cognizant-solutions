0b2400e88d5cb8521a4caed95242ecd00dbaa917
Partha Sarathi Panda <parthasarathipanda2706@gmail.com>
2025-08-03 12:57:04 +0530

    Added task-03

---
+++ Week-6-7/task-03/src/App.jsx
@@ -0,0 +1,14 @@
+// src/App.js
+import React from 'react';
+import CalculateScore from './Components/CalculateScore';
+
+function App() {
+  return (
+    <div>
+      <h1>Student Management Portal</h1>
+      <CalculateScore />
+    </div>
+  );
+}
+
+export default App;
